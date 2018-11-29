b4de070154b193de98094cff92604f6fc8fa05f0
danielkwok21 <danielzhengxian21@gmail.com>
2018-11-29 15:16:21 +0800

    able to plot centroids

--- script.py
+++ script.py
@@ -3,39 +3,44 @@ import Util as u
 import MachineLearning as ml
 import numpy
 import cv2
-import math
+import json
+from matplotlib import pyplot as plt
 
-ori = cv2.imread('samples/sample11.jpg')
-ori = ip.resize(ori, 0.3)
+ori = cv2.imread('samples/sample0.jpg')
 img = ori
+
+wordObjects = json.load(open('samples/wordObjects.json'))
+
+
+for w in wordObjects:
+	if w['field']!='':		
+		# topleft
+		x1 = w['vertices'][3]['x']
+		y1 = w['vertices'][3]['y']
+
+		#bottom right
+		x2 = w['vertices'][1]['x']
+		y2 = w['vertices'][1]['y']
+
+		topLeft = (x1, y1)
+		bottomRight = (x2, y2)
+
+		# draw rect from json	
+		cv2.rectangle(img, topLeft, bottomRight, (0,255,0), 3)
+
+		#find centroid
+		mid_x = (x1+x2)/2
+		mid_y = (y1+y2)/2
+
+		cv2.circle(img, (mid_x, mid_y), 6, (255,0,0), -1)
+
+		# plot centroids w diff colour according to fields
+		if(w['field']=='location'):
+			plt.scatter(mid_x,mid_y,20,'b','o')
+		elif(w['field']=='person'):
+			plt.scatter(mid_x,mid_y,20,'g','o')
+
+
+img = ip.resize(img, 0.35)
 ip.displayImage(img)
-
-img = cv2.blur(img, (7,7))
-img = ip.cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-thresh, img = ip.binarize(img)
-ip.displayImage(img)
-
-img = cv2.Canny(img, 150, 200)
-ip.displayImage(img)
-
-img = ip.dilate(img, 5)
-ip.displayImage(img)
-
-#get x amount of smallest contours
-contours = ip.getContours(img)
-x = len(contours)
-print x
-
-sensitivity = int(numpy.ceil(x/4))
-print sensitivity
-
-contours = ip.getTopContours(contours, sensitivity)
-u.writeToFile(contours, 'contours')
-
-left, top, right, bottom = ip.drawRects(ori, contours)
-
-cropped = ori[top:bottom, left:right]
-ip.displayImage(cropped, "cropped")
-
-# ml.kmeans(contours, 5)
-
+plt.show()
