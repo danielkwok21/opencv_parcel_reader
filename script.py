7254c99d264da91c43d7e54711aedb42cb96099b
danielkwok21 <danielzhengxian21@gmail.com>
2018-12-09 18:02:02 +0800

    beautify for interim report

--- script.py
+++ script.py
@@ -1,39 +1,29 @@
 import ImageProcessing as ip
 import Util as u
-import MachineLearning as ml
-import numpy
 import cv2
-import math
 
-ori = cv2.imread('samples/sample0.jpg')
+ori = cv2.imread('samples/test.jpg')
 img = ori
 
+# binarize and smoothening
 img = ip.cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 thresh, img = ip.binarize(img)
 img = cv2.bilateralFilter(img,9,100,100)
-temp = ip.resize(img, 0.3);
-ip.displayImage(temp);
 
+# edge detection by canny & dilate all edges
 img = cv2.Canny(img, 150, 200)
 img = ip.dilate(img, 10)
-temp = ip.resize(img, 0.3);
-ip.displayImage(temp);
 
-#get x amount of smallest contours
+# extracts all contours from image
+# writes it to a file to store
+# crops a rectangle to encapsulate all contours
 contours = ip.getContours(img)
-# x = len(contours)
-# sensitivity = int(numpy.ceil(x/2))
-# print sensitivity
-
-# contours = ip.getTopContours(contours, sensitivity)
 u.writeToFile(contours, 'contours')
-
 left, top, right, bottom = ip.drawRects(ori, contours)
-
 cropped = ori[top:bottom, left:right]
 
+# resize and display
 cropped = ip.resize(cropped, 0.3)
 ip.displayImage(cropped, "cropped")
 
-# ml.kmeans(contours, 5)
 
