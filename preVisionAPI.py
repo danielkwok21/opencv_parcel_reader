875c38a6f606046c0d4a47e1ef03c9f7888d766a
danielkwok21 <danielzhengxian21@gmail.com>
2018-12-26 23:34:29 +0800

    trying hls and hsv

--- preVisionAPI.py
+++ preVisionAPI.py
@@ -5,26 +5,22 @@ import numpy as np
 # load ori image
 ori = cv2.imread('samples/sample2.jpg', cv2.IMREAD_COLOR)
 img = ori
-temp = ip.resize(img, 0.3)
-ip.displayImage(temp, 'ori')
 
-# change from bgr to lab
-img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+# change from bgr to hls
+img = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
 
 # extract l channel
-l, a, b = cv2.split(img)
+h, l, s = cv2.split(img)
 
 # adaptive histogram equalization
 clahe = cv2.createCLAHE(clipLimit=3, tileGridSize=(4,4))
 cl = clahe.apply(l)
 
-# remerge to create new lab
-img = cv2.merge((cl, a, b))
+# remerge
+img = cv2.merge((h, cl, s))
 
-# convert lab to rgb
-img = cv2.cvtColor(img, cv2.COLOR_LAB2BGR)
-temp = ip.resize(img, 0.3)
-ip.displayImage(temp, 'rgb')
+# convert hls to rgb
+img = cv2.cvtColor(img, cv2.COLOR_HLS2BGR)
 
 # convert rgb to hsv (to remove orange words)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
